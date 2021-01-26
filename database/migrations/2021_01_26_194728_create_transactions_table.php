30bc5c961600831dccbd24253cfd833477314c11
Oketa Fred <oketafred@gmail.com>
2021-01-26 22:51:58 +0300

    Aded Transaction Model Controller and Migration

---
+++ database/migrations/2021_01_26_194728_create_transactions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->string('wallet_id');
+            $table->double('amount');
+            $table->string('description');
+            $table->string('transaction_type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
