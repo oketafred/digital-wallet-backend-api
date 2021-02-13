9603aa23c1bf890daf4544fa1b5c9c0177407d90
Oketa Fred <oketafred@gmail.com>
2021-02-13 17:48:53 +0300

    Added Wallet Creation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -5,25 +5,37 @@ namespace App\Http\Controllers;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
+use App\Models\Wallet;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Symfony\Component\HttpFoundation\Response;
+use Webpatser\Uuid\Uuid;
+use Propaganistas\LaravelPhone\PhoneNumber;
 
 class AuthController extends Controller
 {
     public function register(RegisterRequest $request)
     {
-        $user = User::create([
-            'first_name'    => $request->input('first_name'), 
-            'last_name'     => $request->input('last_name'), 
-            'email'         => $request->input('email'), 
-            'phone_number'  => $request->input('phone_number'),
-            'password'      => Hash::make($request->input('password'))
-        ]);
-
-        return response()->json($user);
+        $registration = DB::transaction(function () use($request) {
+            $user = User::create([
+                'first_name'    => $request->input('first_name'), 
+                'last_name'     => $request->input('last_name'), 
+                'email'         => $request->input('email'), 
+                'phone_number'  => (string) PhoneNumber::make($request->input('phone_number'), 'UG')->formatE164(),
+                'password'      => Hash::make($request->input('password'))
+            ]);
+            
+            $wallet = Wallet::create([
+                'user_id' => $user->id,
+                'wallet_no' => Uuid::generate(),
+                'account_balance' => 0
+            ]);
+            return compact('user', 'wallet');
+        });
+        return response()->json($registration['user']);
     }
 
     public function login(LoginRequest $request) 
